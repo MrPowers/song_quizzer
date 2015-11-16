85f7d9505ae008e30c7e3d3ae4a2b9b070ed3292
MrPowers <matthewkevinpowers@gmail.com>
2015-11-15 19:38:12 -0500

    Add Font Awesome check marks and times x's to indicate correct/incorrect answers

--- public/javascripts/song_quiz.js
+++ public/javascripts/song_quiz.js
@@ -1,20 +1,36 @@
 $(document).ready(function() {
-  function colorize(input) {
+
+  function checkCorrectness(input) {
     input.focus(function() {
-      console.log('in');
+      // can add stuff here
     }).blur(function() {
-        var v = $(this).val();
-        var a = $(this).data("answer");
-        if (v === a) {
+        var userAnswer = $(this).val();
+        var correctAnswer = $(this).data("answer");
+        if (userAnswer === "") return;
+        if (userAnswer === correctAnswer) {
           $(this).addClass("correct");
           $(this).removeClass("incorrect");
         } else {
           $(this).addClass("incorrect");
           $(this).removeClass("correct");
         }
+        markCorrectness(input);
     });
   }
+
+  function markCorrectness($input) {
+    var s = $input.next("span");
+    if ($input.hasClass("correct")) {
+      s.replaceWith("<span class='correct fa-stack fa-1x'><i class='fa fa-check-circle fa-2x'></i></span>")
+    } else if ($input.hasClass("incorrect")) {
+      s.replaceWith("<span class='incorrect fa-stack fa-1x'><i class='fa fa-times-circle fa-2x'></i></span>")
+    }
+  }
+
   $.each($("input"), function( index, value ) {
-    colorize($(value));
+    // add spans after all the input boxes for the markCorrectness() function
+    $("<span></span>").insertAfter($(value))
+    checkCorrectness($(value));
   });
+
 });
