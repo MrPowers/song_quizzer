36ab8d293e9a7465f4493c804e7e63717ed8864c
MrPowers <matthewkevinpowers@gmail.com>
2015-11-16 21:32:24 -0500

    Delete the comment about the tooltip bug

--- public/javascripts/song_quiz.js
+++ public/javascripts/song_quiz.js
@@ -26,7 +26,6 @@ $(document).ready(function() {
     } else if ($input.hasClass("incorrect")) {
       var correctAnswer = $input.data("answer");
       var incorrectCross = "<span class='incorrect fa-stack fa-1x'><i class='fa fa-times-circle fa-2x'></i></span>"
-      //There's some issue with the help variable's use of correctAnswer. If the correct answer has one or more spaces, the tooltip only displays the text before the first space. For example, in the song Sunset, "se escondió" displays as "se."
       var help = "<span data-toggle='tooltip' data-placement='top' data-original-title='" + correctAnswer + "' class='fa-stack fa-1x'><i class='fa fa-question-circle fa-2x'></i></span>"
       s.html(incorrectCross + help)
       $('[data-toggle="tooltip"]').tooltip();
