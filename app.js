806192e98fb6ddb53fbbc74269792b7f88e11e60
MrPowers <matthewkevinpowers@gmail.com>
2017-01-10 13:36:10 -0500

    Delete some comments

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const express = require('express'),
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -25,17 +24,13 @@ app.use("/blog", require(path.join(__dirname, "controllers", "blogs_controller.j
 app.use("/lyrics", require(path.join(__dirname, "controllers", "lyrics_controller.js"))());
 app.use("/", require(path.join(__dirname, "controllers", "songs_controller.js"))());
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
 // development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -47,7 +42,6 @@ if (app.get('env') === 'development') {
 }
 
 // production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -55,6 +49,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
