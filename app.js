726b1ecaad5d56548581a8614773ecd888c2a3bc
MrPowers <matthewkevinpowers@gmail.com>
2017-01-10 12:15:01 -0500

    Replace some vars with consts

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 /*jslint node: true */
 "use strict";
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var favicon = require('serve-favicon');
+const express = require('express'),
+      path = require('path'),
+      logger = require('morgan'),
+      cookieParser = require('cookie-parser'),
+      bodyParser = require('body-parser'),
+      favicon = require('serve-favicon');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -62,7 +59,6 @@ app.get('/language-tutor', function(request, response) {
 });
 
 var fs = require('fs');
-var path = require('path');
 
 app.get('/:artist/:song', function(request, response) {
   var foundSong = appdata.songs.find(s => s.name === request.params.song);
