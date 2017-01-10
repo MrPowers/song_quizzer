c890ef5b413d0a5132ebf68d67d2048f3e33fc47
MrPowers <matthewkevinpowers@gmail.com>
2017-01-10 09:44:34 -0500

    Removed a route that wasn't being used

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
 
-var routes = require('./routes/index');
-
 var app = express();
 
 // view engine setup
