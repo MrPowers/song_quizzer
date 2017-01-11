ee47e8b17d4d6153789d58fed057a3928a3ff824
MrPowers <matthewkevinpowers@gmail.com>
2017-01-11 08:06:40 -0500

    move the logic to fetch the songs to a standalone method

--- controllers/songs_controller.js
+++ controllers/songs_controller.js
@@ -3,30 +3,34 @@ const Router = require("express").Router,
       songCategories = require("../data/song_categories.json").categories,
       _ = require("lodash");
 
+function chunkedSongs() {
+  const result = songCategories.map(function(c) {
+    const songs = appdata.songs.filter( s =>
+      s.language === c.language && s.genre === c.genre && s.hot === c.hot
+    );
+    const shuffledSongs = _.shuffle(songs);
+    return {
+      all: _.chunk(shuffledSongs, 4),
+      category: c,
+      id: c.language + "-" + c.genre + "-" + c.hot
+    };
+  });
+  return result;
+}
+
 module.exports = function() {
   const router = new Router();
 
   router.route('/').get(function(request, response) {
-    var data = songCategories.map(function(c) {
-      var songs = appdata.songs.filter( s =>
-        s.language === c.language && s.genre === c.genre && s.hot === c.hot
-      );
-      var shuffledSongs = _.shuffle(songs);
-      return {
-        all: _.chunk(shuffledSongs, 4),
-        category: c,
-        id: c.language + "-" + c.genre + "-" + c.hot
-      };
-    });
     response.render('index', {
-      songData: data,
+      songData: chunkedSongs(),
       showLanguageFilter: true,
       showDifficultyFilter: false
     });
   });
 
   router.route('/:artist/:song').get(function(request, response) {
-    var foundSong = appdata.songs.find(s => s.name === request.params.song);
+    const foundSong = appdata.songs.find(s => s.name === request.params.song);
     response.render('smart_song_quiz', {
       song: foundSong,
       showLanguageFilter: false,
